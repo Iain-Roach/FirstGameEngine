e16f87c74085d1c2256de91287f2fc1379fdf2e9
Iain-Roach <Roach.iain@gmail.com>
2024-02-08 19:27:02 -0500

    started adding d2d stufff

--- FirstEngine/src/FirstEngine/EngineApp.h
+++ FirstEngine/src/FirstEngine/EngineApp.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "EngineCore.h"
 
-
 namespace Ferrus {
 
 	class FERRUS_API EngineApp
@@ -10,11 +9,42 @@ namespace Ferrus {
 		EngineApp();
 		virtual ~EngineApp();
 		void Run();
+
+		// Register the window class and call methods for instantiating
+		HRESULT Initialize();
+
+		// Process and dispatch messages || May move this to dedicated error handling section
+		void RunMessageLoop();
 	private:
+		// Initialize device-independent resources.
+		HRESULT CreateDeviceIndependentResources();
+
+		// Initialize device-dependent resources.
+		HRESULT CreateDeviceResources();
+
+
+		// Release device-dependent resource.
+		void DiscardDeviceResources();
+
+		// Draw Content.
+		HRESULT OnRender();
+
+		// Resize the render target.
+		void OnResize(UINT width,
+			 UINT height);
+
+		// The windows procedure.
+		static LRESULT CALLBACK WndProc(
+			HWND hWnd,
+			UINT message,
+			WPARAM wParam,
+			LPARAM lParam
+		);
 
 	};
 
 
+	// Create App here
 	EngineApp* CreateApplication();
 
 }
