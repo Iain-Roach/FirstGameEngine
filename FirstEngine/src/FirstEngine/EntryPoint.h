164cc56de4b216c11e13f595ce671bfb5b7dcaa3
Iain-Roach <Roach.iain@gmail.com>
2024-02-20 18:03:56 -0500

    working on fixing and drawing objects from test scene

--- FirstEngine/src/FirstEngine/EntryPoint.h
+++ FirstEngine/src/FirstEngine/EntryPoint.h
@@ -7,10 +7,10 @@ extern Ferrus::EngineApp* Ferrus::CreateApplication();
 
 int main(int argc, char** argv) 
 {
-	printf("Hello World\n");
+	/*printf("Hello World\n");
 	auto app = Ferrus::CreateApplication();
 	app->Run();
-	delete app;
+	delete app;*/
 }
 
 int WINAPI wWinMain(
@@ -19,6 +19,26 @@ int WINAPI wWinMain(
 	LPWSTR cmd,
 	int nCmdShow)
 {
-	return 0;
+    // Use HeapSetInformation to specify that the process should
+    // terminate if the heap manager detects an error in any heap used
+    // by the process.
+    // The return value is ignored, because we want to continue running in the
+    // unlikely event that HeapSetInformation fails.
+    HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
+
+    if (SUCCEEDED(CoInitialize(NULL)))
+    {
+        {
+            auto app = Ferrus::CreateApplication();
+
+            if (SUCCEEDED(app->Initialize()))
+            {
+                app->RunMessageLoop();
+            }
+        }
+        CoUninitialize();
+    }
+
+    return 0;
 }
 #endif
